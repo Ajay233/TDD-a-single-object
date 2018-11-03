6df6a6de2a22cadd4320e531d2b00b09c6ef47a0
Ajay <ajaymungur@hotmail.com>
2018-11-03 17:10:41 +0000

    commit 3 - Used TDD to update roll method so it returns a number between 1 and 6

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,10 +1,19 @@
 require 'dice'
 
 describe Dice do
+  let(:dice) {Dice.new}
+
 
   it "creates a die" do
-    dice = Dice.new
     expect(dice).to be_instance_of(Dice)
+  end
+
+  it "can roll a die" do
+    expect(dice).to respond_to(:roll)
+  end
+
+  it "outputs a number between 1 and 6 when dice rolled" do
+    expect(dice.roll).to eq 3
   end
 
 end
